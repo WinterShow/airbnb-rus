1455254c7a1e28499e22c661259a12efd263418c
Федор Ендовицкий <fedor.endovitskiy@rmg-region.ru>
2023-12-17 15:45:30 +0300

    Исправлено наименование поля

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -3,21 +3,21 @@ import { PlaceService } from './place.service';
 
 @Controller('room')
 export class PlaceController {
-  constructor(private roomService: PlaceService) {}
+  constructor(private placeService: PlaceService) {}
   @Get('get')
   async get() {
-    return this.roomService.get();
+    return this.placeService.get();
   }
   @Post('create')
   async create() {
-    return this.roomService.create();
+    return this.placeService.create();
   }
   @Post('update')
   async update() {
-    return this.roomService.update();
+    return this.placeService.update();
   }
   @Delete('delete')
   async delete() {
-    return this.roomService.delete();
+    return this.placeService.delete();
   }
 }
